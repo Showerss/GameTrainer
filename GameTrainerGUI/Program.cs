6091018ff201ba23b812a388bab430f56b4ce65d
Phillip Banky <phillip.banky@gmail.com>
2025-06-12 13:21:23 -0700

    did the initial framework for making a godmode checkbox and then deleted my mainwindow from being in program.cs

--- GameTrainerGUI/Program.cs
+++ GameTrainerGUI/Program.cs
@@ -1,29 +1,17 @@
 // Basic resizable GUI window in C# using Windows Forms
-
-using System;
-using System.Drawing;
-using System.Windows.Forms;
+// program.cs launches form1, so we don't need to edit this muuch
 
 namespace GameTrainerGUI
 {
     public class MainWindow : Form
     {
-        public MainWindow()
-        {
-            this.Text = "Game Trainer Base Window";
-            this.Size = new Size(800, 600);
-            this.MinimumSize = new Size(400, 300);
-            this.FormBorderStyle = FormBorderStyle.Sizable;
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.BackColor = Color.FromArgb(30, 30, 30);
-        }
 
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            Application.Run(new Form1());
         }
     }
 }
