72755068708494e80961403b3885eeb5a3aa7af9
Phillip Banky <phillip.banky@gmail.com>
2025-12-06 07:45:42 -0800

    feat: Implement core game trainer logic with C++ memory access and Python integration

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# setup.py
+from setuptools import setup, Extension
 from setuptools import setup, Extension
 
 setup(
@@ -7,9 +7,10 @@ setup(
     packages=["src.python.gui", "src.python.core"],
     ext_modules=[
         Extension(
-            "gametrainer.clib",
-            ["src/c/memory_manager.c", "src/c/input.c"],
+            "src.python.core.clib",
+            ["src/cpp/memory_manager.cpp", "src/cpp/input.cpp", "src/cpp/py_init.cpp"],
             include_dirs=["include"],
+            libraries=["user32", "kernel32"],
         )
     ],
 )
