954a5a82c5717ce7515fb401e0b66ef4b33cad97
Phillip Banky <phillip.banky@gmail.com>
2025-12-07 19:57:06 -0800

    switching over a bunch of c files to c++

--- src/c/interface/interface.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by phill on 7/28/2025.
-//
-
-#ifndef INTERFACE_H
-#define INTERFACE_H
-
-#endif //INTERFACE_H
--- src/c/process_handler.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by phill on 7/28/2025.
-//
-
-#include "process_handler.h"
